fa4a48bb9c984a05615504b9d5af881d78f97d83
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-02-17 23:51:34 +0100

    Commenting out currentExecSpace

--- src/expr/executionSpace.hpp
+++ src/expr/executionSpace.hpp
@@ -6,16 +6,16 @@ namespace esnort
   enum ExecutionSpace{EXEC_HOST,EXEC_DEVICE,EXEC_UNDEFINED};
   enum ExecutionSpaceChangeCost{EXEC_SPACE_CHANGE_COSTS_NOTHING,EXEC_SPACE_CHANGE_COSTS_LITTLE,EXEC_SPACE_CHANGE_COSTS_ALOT};
   
-  constexpr ExecutionSpace currentExecSpace()
-  {
-    return
-#ifdef __CUDA_ARCH__
-      EXEC_DEVICE
-#else
-      EXEC_HOST
-#endif
-      ;
-  }
+//   constexpr ExecutionSpace currentExecSpace()
+//   {
+//     return
+// #ifdef __CUDA_ARCH__
+//       EXEC_DEVICE
+// #else
+//       EXEC_HOST
+// #endif
+//       ;
+//   }
   
   enum ExecutionSpaceAssignmentType{ASSIGN_MATCHING_SPACE,ASSIGN_MISMATCHING_SPACE};
   
