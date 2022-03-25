ba3604872ff8348a0264092df51b8e952f75ab3e
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-03-25 14:56:43 +0100

    Reenabled executiontest

--- src/testExecutionSpace.cpp
+++ src/testExecutionSpace.cpp
@@ -41,132 +41,102 @@ int j;
   using SpinRow=Spin<RwCl::ROW,0>;
   using SpanRow=Span<RwCl::ROW,0>;
 
-StackTens<OfComps<SpinRow>,double> dt;
-StackTens<OfComps<SpinRow>,double> pt;
-
-
-void testSimdifiedAssign()
-{
-  ASM_BOOKMARK_BEGIN("TEST_SIMDIFIED_ASSIGN");
-  dt.simdify()=pt.simdify();
-  ASM_BOOKMARK_END("TEST_SIMDIFIED_ASSIGN");
-}
-
 int in_main(int narg,char** arg)
 {
-  device::initialize(Mpi::rank);
+  // ASM_BOOKMARK_BEGIN("TEST_INDEX");
+  // j=index(CompsList<SpaceTime>{5},SpinRow{3},SpaceTime{1});
+  // LOGGER<<j;
+  // ASM_BOOKMARK_END("TEST_INDEX");
+  // LOGGER<<j;
   
-    ASM_BOOKMARK_BEGIN("TEST_INDEX");
-  __asm volatile ("");
+  // LOGGER<<"Begin assign";
   
-  using C=std::tuple<SpinRow,SpaceTime>;
-  
-  
-  ASM_BOOKMARK_BEGIN("TEST_INDEX");
-  j=index(CompsList<SpaceTime>{5},SpinRow{3},SpaceTime{1});
-  LOGGER<<j;
-  ASM_BOOKMARK_END("TEST_INDEX");
-  LOGGER<<j;
-  
-  // using I=IndexComputer<C>;
-  
-  // I i(SpaceTime(10));
-
-  LOGGER<<"Begin assign";
+  StackTens<OfComps<SpinRow>,double> hostTens;
+  for(SpinRow st=0;st<4;st++)
+    hostTens(st)=st();
   
   for(SpinRow st=0;st<4;st++)
-    pt(st)=st();
+    LOGGER<<st()<<" "<<hostTens(st);
   
-  //testSimdifiedAssign();
+  /// Create
+  DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::DEVICE> deviceTens;
+  deviceTens=hostTens;
   
-  for(SpinRow st=0;st<4;st++)
-    LOGGER<<st()<<" "<<dt(st);
-  
-  // {
-  //   DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::HOST> dt;
-  //   StackTens<OfComps<SpinRow>,double> st;
-  //   st=dt;
-  //   st.fillDynamicTens();
-  // }
-  
-  DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::DEVICE> dtg;
-  auto odt=dt.getRef().getCopyOnExecSpace<ExecutionSpace::DEVICE>();
-  dtg=odt;
-  
-  DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::HOST> dtd;
-  dtd=dtg.getCopyOnExecSpace<ExecutionSpace::HOST>();
+  StackTens<OfComps<SpinRow>,double> backHostTens;
+  backHostTens=deviceTens;
+  //dtd=dtg.getCopyOnExecSpace<ExecutionSpace::HOST>();
   
   for(SpinRow st=0;st<4;st++)
-    LOGGER<<st()<<" "<<dtd(st);
+    LOGGER<<st()<<" "<<backHostTens(st);
   
-  {
-    DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::HOST> dt(SpaceTime{3});
-    for(SpinRow s=0;s<4;s++)
-      for(SpaceTime st=0;st<3;st++)
-	dt(st,s)=st()+3*s;
+//   {
+//     DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::HOST> dt(SpaceTime{3});
+//     for(SpinRow s=0;s<4;s++)
+//       for(SpaceTime st=0;st<3;st++)
+// 	dt(st,s)=st()+3*s;
     
-    DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::HOST> td(SpaceTime{3});
+//     DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::HOST> td(SpaceTime{3});
     
-    td=dt;
+//     td=dt;
     
-    for(SpinRow s=0;s<4;s++)
-      for(SpaceTime st=0;st<3;st++)
-	LOGGER<<td(st,s)<<" "<<st()+3*s;
-  }
+//     for(SpinRow s=0;s<4;s++)
+//       for(SpaceTime st=0;st<3;st++)
+// 	LOGGER<<td(st,s)<<" "<<st()+3*s;
+//   }
   
-  {
-    DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::HOST> t(SpaceTime{3});
-    for(SpinRow s=0;s<4;s++)
-      for(SpaceTime st=0;st<3;st++)
-	t(st,s)=st()+3*s;
-    DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::DEVICE> dt(SpaceTime{3});
-    dt=t;
+//   {
+//     DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::HOST> t(SpaceTime{3});
+//     for(SpinRow s=0;s<4;s++)
+//       for(SpaceTime st=0;st<3;st++)
+// 	t(st,s)=st()+3*s;
+//     DynamicTens<OfComps<SpinRow,SpaceTime>,double,ExecutionSpace::DEVICE> dt(SpaceTime{3});
+//     dt=t;
     
-    DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::DEVICE> td(SpaceTime{3});
+//     DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::DEVICE> td(SpaceTime{3});
     
-    td=dt;
+//     td=dt;
     
-    DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::HOST> d(SpaceTime{3});
+//     DynamicTens<OfComps<SpaceTime,SpinRow>,double,ExecutionSpace::HOST> d(SpaceTime{3});
     
-    d=td;
-    for(SpinRow s=0;s<4;s++)
-      for(SpaceTime st=0;st<3;st++)
-	LOGGER<<d(st,s)<<" "<<st()+3*s;
-  }
+//     d=td;
+//     for(SpinRow s=0;s<4;s++)
+//       for(SpaceTime st=0;st<3;st++)
+// 	LOGGER<<d(st,s)<<" "<<st()+3*s;
+//   }
   
-  return 0;
+//   return 0;
   
-  StackTens<OfComps<SpinRow>,double> st;
-  DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::HOST> dst;
-  st=dst;
+//   StackTens<OfComps<SpinRow>,double> st;
+//   DynamicTens<OfComps<SpinRow>,double,ExecutionSpace::HOST> dst;
+//   st=dst;
   
-  dtg=dt;
+//   dtg=dt;
   
-  {
-    auto rdt=dt.getRef();
-    rdt(SpinRow{3})=0;
-  }
-  //dt(SpinRow(0))=9;
-  st(SpinRow(2));
-  LOGGER<<"AAAA";
-  auto r=
-      TupleDiscriminate<SizeIsKnownAtCompileTime, C>::Valid{};
+//   {
+//     auto rdt=dt.getRef();
+//     rdt(SpinRow{3})=0;
+//   }
+//   //dt(SpinRow(0))=9;
+//   st(SpinRow(2));
+//   LOGGER<<"AAAA";
+//   auto r=
+//       TupleDiscriminate<SizeIsKnownAtCompileTime, C>::Valid{};
   
-  Spin<RwCl::ROW,0> s(1);
-  s=1;
-  // auto rr=Transp<Spin<RwCl::ROW>>::type{};
+//   Spin<RwCl::ROW,0> s(1);
+//   s=1;
+//   // auto rr=Transp<Spin<RwCl::ROW>>::type{};
   
-  // decltype(s)::Transp t;
-  auto t=s.transp();
+//   // decltype(s)::Transp t;
+//   auto t=s.transp();
   
-  // SpinRow st;
-  // auto _st=st.transp();
+//   // SpinRow st;
+//   // auto _st=st.transp();
   
-  s.sizeAtCompileTimeAssertingNotDynamic();
+//   s.sizeAtCompileTimeAssertingNotDynamic();
   
-#if not COMPILING_FOR_DEVICE
-  static_assert(StackedVariable<int>::execSpace==esnort::ExecutionSpace::HOST,"We are issuing A on the host");
-#endif
+// #if not COMPILING_FOR_DEVICE
+//   static_assert(StackedVariable<int>::execSpace==esnort::ExecutionSpace::HOST,"We are issuing A on the host");
+// #endif
   
   // ASM_BOOKMARK_BEGIN("TEST_ASSIGN");
   
