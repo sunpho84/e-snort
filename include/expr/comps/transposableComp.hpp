c9cb305c7374fb801e39c2c96752ba3593a0e9e3
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-04-03 01:53:16 +0200

    Adding check on rw/cln

--- include/expr/comps/transposableComp.hpp
+++ include/expr/comps/transposableComp.hpp
@@ -9,45 +9,73 @@
 
 #include <expr/comps/compRwCl.hpp>
 
-namespace esnort::compFeat
+namespace esnort
 {
-  enum class IsTransposable{FALSE,TRUE};
-  
-  template <IsTransposable _IsT,
-	    typename _C>
-  struct Transposable;
-  
-  template <RwCl _RC,
-	    int _Which,
-	    template <RwCl,int> typename _C>
-  struct Transposable<IsTransposable::TRUE,_C<_RC,_Which>>
+  namespace compFeat
   {
-    static constexpr bool isTransposable=true;
+    enum class IsTransposable{FALSE,TRUE};
     
-    static constexpr IsTransposable Transposability=IsTransposable::TRUE;
+    template <IsTransposable _IsT,
+	      typename _C>
+    struct Transposable;
     
-    static constexpr RwCl RC=_RC;
+    template <RwCl _RC,
+	      int _Which,
+	      template <RwCl,int> typename _C>
+    struct Transposable<IsTransposable::TRUE,_C<_RC,_Which>>
+    {
+      static constexpr bool isTransposable=true;
+      
+      static constexpr IsTransposable Transposability=IsTransposable::TRUE;
+      
+      static constexpr RwCl RC=_RC;
+      
+      static constexpr RwCl transpRc=transpRwCl<RC>;
+      
+      static constexpr int Which=_Which;
+      
+      using Transp=_C<transpRc,_Which>;
+      
+      static constexpr bool isMatr=true;
+    };
     
-    static constexpr RwCl transpRc=transpRwCl<RC>;
-    
-    static constexpr int Which=_Which;
-    
-    using Transp=_C<transpRc,_Which>;
-    
-    static constexpr bool isMatr=true;
-  };
+    template <typename _C>
+    struct Transposable<IsTransposable::FALSE,_C>
+    {
+      static constexpr bool isTransposable=false;
+      
+      static constexpr IsTransposable Transposability=IsTransposable::FALSE;
+      
+      using Transp=_C;
+      
+      static constexpr bool isMatr=false;
+    };
+  }
   
-  template <typename _C>
-  struct Transposable<IsTransposable::FALSE,_C>
+  namespace internal
   {
-    static constexpr bool isTransposable=false;
-    
-    static constexpr IsTransposable Transposability=IsTransposable::FALSE;
-    
-    using Transp=_C;
-    
-    static constexpr bool isMatr=false;
-  };
+    /// Determine if a component is of row or column type
+    template <typename T,
+	      RwCl RC>
+    static constexpr
+    bool _transposableCompIsOfRwCl()
+    {
+      if constexpr(T::isTransposable)
+	return (T::RC==RC);
+      else
+	return false;
+    }
+  }
+  
+  /// Determine if a component is of row type
+  template <typename T>
+  static constexpr bool isRow=
+    internal::_transposableCompIsOfRwCl<T,RwCl::ROW>();
+  
+  /// Determine if a component is of cln type
+  template <typename T>
+  static constexpr bool isCln=
+    internal::_transposableCompIsOfRwCl<T,RwCl::CLN>();
 }
 
 #endif
